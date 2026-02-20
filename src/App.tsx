27bc70e95f15e8d9cc5b073b34f65052858205ad
Raymond Toyosato <raymondxiao@cmail.carleton.ca>
2026-02-20 20:47:25 +0000

    fix: Use different base path for dev and production

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import './App.css'
 
+const basename = import.meta.env.PROD ? '/group-projects-backdoor-corp' : ''
+
 function App() {
   return (
-    <BrowserRouter basename="/group-projects-backdoor-corp">
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
