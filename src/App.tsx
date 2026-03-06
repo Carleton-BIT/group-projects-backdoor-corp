8e7a5b127ae9ca2202c42e425f5b7ef86f4342f8
ismailfarahh3-cmyk <ismail.farahh3@gmail.com>
2026-03-06 16:03:45 -0500

    add navagation button to return to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import SyllabusPage from './pages/SyllabusPage'
+import Calendar from './pages/Calendar' // 1. Added your import
 import './App.css'
 
 const basename = import.meta.env.PROD ? '/group-projects-backdoor-corp' : ''
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/syllabus" element={<SyllabusPage />} />
+        <Route path="/calendar" element={<Calendar />} /> {/* 2. Added your route */}
       </Routes>
     </BrowserRouter>
   )
